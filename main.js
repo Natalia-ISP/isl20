8beea923bbfa84f6b315188e0f443ddf315ccc8f
natalia <ponomarevanatasha16072007@gmail.com>
2026-05-14 22:37:43 +0300

    Step 5: click event handling

--- main.js
+++ main.js
@@ -14,3 +14,11 @@ title.style.padding = "10px";
 text.style.fontSize = "18px";
 text.style.fontSize = "italic";
 text.style.color = "darkgreen";
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    alert("кнопка нажата!");
+    btn.style.backgroundColor = "#4CAF50";
+    btn.style.color = "white";
+});
